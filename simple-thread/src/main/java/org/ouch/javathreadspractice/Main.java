563d3dc333146f46fcfe779a326ee2212a425c97
Yuri Laptev <yurilap4@gmail.com>
2022-08-10 17:21:06 +0300

    First task: 'simple-thread' project

--- simple-thread/src/main/java/org/ouch/javathreadspractice/Main.java
+++ simple-thread/src/main/java/org/ouch/javathreadspractice/Main.java
@@ -1,7 +1,11 @@
 package org.ouch.javathreadspractice;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        NewThread newThread = new NewThread();
+
+        newThread.start();
+
+        newThread.join();
     }
 }
